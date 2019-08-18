73daed8c3bfa9fe8c8517caf68b3049dbc80425c
kou0 <ialove1115>
2019-08-18 19:16:23 +0900

    remove db

--- app/src/main/java/com/example/tensor/MainActivity.java
+++ app/src/main/java/com/example/tensor/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.tensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
-    private DBOpenHelper helper;
+
+    private static final int MONSTER_DATA_NUM = 12;
+    private static final int ELEMENT_NUM = 2;
+    String[][] monster_data = new String[MONSTER_DATA_NUM][ELEMENT_NUM];
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,29 +31,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //最初の画面を表示するメソッド呼び出し。
         SetFirstScreen();
-
     }
 
     private void SetFirstScreen() {
 
         setContentView(R.layout.activity_main);
-        //条件ボタン
-        Button termbutton = findViewById(R.id.term);
-        //スキルターン
-        TextView minitern = findViewById(R.id.minitern);
-        TextView maxtern = findViewById(R.id.maxtern);
-        //イベント追加
-        termbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //条件決定の画面へ
-                SetTermScreen();
-            }
-        });
         //検索ボタン
-        Button searchbutton = findViewById(R.id.search);
+        Button search_button = findViewById(R.id.search);
         //イベント追加
-        searchbutton.setOnClickListener(new View.OnClickListener() {
+        search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //入力されたdataを渡す処理が必要？
@@ -52,8 +50,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void SetTermScreen() {
-        setContentView(R.layout.terms);
+    private void SetResultScreen(TextView minitern, TextView maxtern) {
+        //入力されたデータを元に表示する処理が必要？
+        setContentView(R.layout.results);
+        //データを読み込む(まだできてません)
+        data_reader();
+        TextView result_mons = findViewById(R.id.result_mons);
+        result_mons.setText("");
+        for(int i=0;i<MONSTER_DATA_NUM;i++){
+            result_mons.append(monster_data[i][0]+"  "+monster_data[i][1]+"\n");
+        }
+
         //初期画面へ戻るボタン
         Button backtomainbutton = findViewById(R.id.backtomain);
         backtomainbutton.setOnClickListener(new View.OnClickListener() {
@@ -64,37 +71,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void SetResultScreen(TextView minitern, TextView maxtern) {
-        //入力されたデータを元に表示する処理が必要？
-        setContentView(R.layout.results);
-        helper = new DBOpenHelper(this);
-        final SQLiteDatabase db = helper.getReadableDatabase();
-        final String[] columns=new String[]{"_id","name","skill_tern"};
-        Cursor c = db.query("MonsterDB",columns, null, null, null, null, null);
-        c.moveToFirst();
+    public void data_reader() {
+        try {
+            // txtファイルの読み込み
+            FileInputStream fileInputStream = openFileInput("data.txt");
+            BufferedReader bufferReader= new BufferedReader(new InputStreamReader(fileInputStream, StandardCharsets.UTF_8));
+            String line;
+            int i = 0;
+            while ((line = bufferReader.readLine()) != null) {
 
-        CharSequence[] list = new CharSequence[c.getCount()];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = c.getString(1);
-            c.moveToNext();
-        }
-        c.close();
+                //スペース区切りで１つづつ配列に入れる
+                String[] RowData = line.split(" ");
 
-        TextView textView0 = findViewById(R.id.text0);
-        TextView textView1 = findViewById(R.id.text1);
-        TextView textView2 = findViewById(R.id.text2);
-        textView0.setText(list[0]);
-        textView1.setText(list[1]);
-        textView2.setText(list[2]);
-        //初期画面へ戻るボタン
-        Button backtomainbutton = findViewById(R.id.backtomain);
-        backtomainbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //最初の画面へ
-                SetFirstScreen();
+                //txtの左([0]番目)から順番にセット
+                monster_data[i][0] = RowData[0];
+                monster_data[i][1] = RowData[1];
+                i++;
             }
-        });
+            bufferReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
