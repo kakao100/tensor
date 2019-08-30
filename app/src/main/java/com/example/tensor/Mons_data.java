6bec8473d687ed43db61c9f05b6345eab628a359
kou0 <ialove1115>
2019-08-30 23:32:10 +0900

    constractor change

--- app/src/main/java/com/example/tensor/Mons_data.java
+++ app/src/main/java/com/example/tensor/Mons_data.java
@@ -24,32 +24,66 @@ public class Mons_data {
     private int shortest_tern;
     private int longest_tern;
     private String skill_exp;
-    private int skill_feature;
+    private String[] skill_feature;
     private int converted_drop;
     private int convert_drop;
     private int leader_skill_id;
     private String leader_skill_name;
-    private int leader_skill_hp;
-    private int leader_skill_attack;
-    private int leader_skill_cure;
-    private int leader_skill_Alle;
+    private String leader_skill_exp;
+    private String[] leader_skill_feature;
+    private double leader_skill_hp;
+    private double leader_skill_attack;
+    private double leader_skill_cure;
+    private double leader_skill_Alle;
     private String[] awa;
 
 
-    public Mons_data(String line){
+    public Mons_data(String line) {
         //図鑑ナンバー(数字),モンスター名(日本語),属性(英語),副属性(英語),レア度(数字),コスト(数字),継承可能か(真偽),タイプ１qタイプ２qタイプ３(日本語),
         // HPステータスq攻撃ステータスq回復ステータス(数字),110HPステータスq110攻撃ステータスq110回復ステータス(数字),スキルID(数字)qスキル名(日本語),
         // 最短スキルターンq最大スキルターン(数字),スキル効果名(日本語),スキル特徴ID(数字)qスキル特徴(日本語),変換元(数字),変換先の色(数字),リータースキルID(数字)qリーダースキル名(日本語),
         // リーダスキル効果(日本語),リーダスキル特徴ID(数字)qリーダスキル特徴(日本語)qリーダスキル特徴ID(数字)qリーダスキル特徴(日本語)q並列,
         // リーダースキル倍率HP,リーダースキル倍率攻撃,リーダースキル倍率回復,軽減の倍率(数字),覚醒(日本語)q覚醒(日本語)q覚醒(日本語),,,,
-        String[] data=line.split(",");
-        this.id = Integer.parseInt(data[0]);
-        this.name = data[1];
-        this.attack = Integer.parseInt(data[2]);
-        this.hp = Integer.parseInt(data[3]);
-        this.cure = Integer.parseInt(data[4]);
-        this.longest_tern = Integer.parseInt(data[5]);
-        this.shortest_tern = Integer.parseInt(data[6]);
+        //1,ティラ,144q71q13,noqnoqno,1qヒートブレス,8,3,自分の攻撃力×10倍の火属性攻撃,1q単体ダメージq3q属性ダメージq,no,no,1q火の力,火属性の攻撃力が1.5倍。ドロップ操作時間を2秒延長。,1q属性倍率q1q攻撃倍率q1q操作時間延長q,0,1.5,0,0,noq,noq,
+        String[] data = line.split(",");
+        String[] temp;
+        id = Integer.parseInt(data[0]);
+        name = data[1];
+        temp = split_q(data[2]);
+        hp = Integer.parseInt(temp[0]);
+        attack = Integer.parseInt(temp[1]);
+        cure = Integer.parseInt(temp[2]);
+        if (data[3].contains("no")){
+            hp_110 = 0;
+            attack_110 = 0;
+            cure_110 = 0;
+        }
+        else{
+            temp = split_q(data[3]);
+            hp_110 = Integer.parseInt(temp[0]);
+            attack_110 = Integer.parseInt(temp[1]);
+            cure_110 = Integer.parseInt(temp[2]);
+        }
+        temp = split_q(data[4]);
+        skill_id = Integer.parseInt(temp[0]);
+        skill_name = temp[1];
+        longest_tern = Integer.parseInt(data[5]);
+        shortest_tern = Integer.parseInt(data[6]);
+        skill_exp=data[7];
+        skill_feature = split_q(data[8]);
+        convert_drop = 0;
+        converted_drop = 0;
+        temp = split_q(data[11]);
+        leader_skill_id = Integer.parseInt(temp[0]);
+        leader_skill_name = temp[1];
+        leader_skill_exp = data[12];
+        leader_skill_feature = split_q(data[13]);
+        //0をparseDouble出来ないのかな、、、
+        //エラーが出ます
+        //leader_skill_hp = Double.parseDouble(data[14]);
+        //leader_skill_attack = Double.parseDouble(data[15]);
+        //leader_skill_cure = Double.parseDouble(data[16]);
+        //leader_skill_Alle = Double.parseDouble(data[17]);
     }
     public int getid(){ return id; }
     public String getname(){ return name; }
@@ -72,17 +106,19 @@ public class Mons_data {
     public int getshortest_tern(){ return shortest_tern; }
     public int getlongest_tern(){ return longest_tern; }
     public String getskill_exp(){ return skill_exp; }
-    public int getskill_feature(){ return skill_feature; }
+    public String[] getskill_feature(){ return skill_feature; }
     public int getconverted_drop(){ return converted_drop; }
     public int getconvert_drop(){ return convert_drop; }
     public int getleader_skill_id(){ return leader_skill_id; }
     public String getleader_skill_name(){ return leader_skill_name; }
-    public int getleader_skill_hp(){ return leader_skill_hp; }
-    public int getleader_skill_attack(){ return leader_skill_attack; }
-    public int getleader_skill_cure(){ return leader_skill_cure; }
-    public int getleader_skill_Alle(){ return leader_skill_Alle; }
+    public String getleader_skill_exp(){ return leader_skill_exp; }
+    public String[] getleader_skill_feature(){ return leader_skill_feature; }
+    public double getleader_skill_hp(){ return leader_skill_hp; }
+    public double getleader_skill_attack(){ return leader_skill_attack; }
+    public double getleader_skill_cure(){ return leader_skill_cure; }
+    public double getleader_skill_Alle(){ return leader_skill_Alle; }
     public String[] getawa(){ return awa; }
-
+    public String[] split_q(String line){return line.split("q");}
 
 
 
