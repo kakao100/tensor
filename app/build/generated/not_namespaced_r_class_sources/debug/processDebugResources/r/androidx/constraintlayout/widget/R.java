7a84f08ca5e7b505b30d9f2d8d3e6861d8dd31e2
kou0 <ialove1115>
2019-09-09 15:34:11 +0900

    覚醒表示

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f080042;
-        public static final int gone = 0x7f08004f;
-        public static final int invisible = 0x7f08005a;
-        public static final int left = 0x7f08005f;
-        public static final int packed = 0x7f080079;
-        public static final int parent = 0x7f08007b;
-        public static final int percent = 0x7f08007e;
-        public static final int right = 0x7f080084;
-        public static final int spread = 0x7f0800ac;
-        public static final int spread_inside = 0x7f0800ad;
-        public static final int start = 0x7f0800b2;
-        public static final int top = 0x7f0800cb;
-        public static final int wrap = 0x7f0800da;
+        public static final int bottom = 0x7f080024;
+        public static final int end = 0x7f080041;
+        public static final int gone = 0x7f08004e;
+        public static final int invisible = 0x7f080059;
+        public static final int left = 0x7f08005e;
+        public static final int packed = 0x7f080078;
+        public static final int parent = 0x7f08007a;
+        public static final int percent = 0x7f08007d;
+        public static final int right = 0x7f080083;
+        public static final int spread = 0x7f0800ab;
+        public static final int spread_inside = 0x7f0800ac;
+        public static final int start = 0x7f0800b1;
+        public static final int top = 0x7f0800ca;
+        public static final int wrap = 0x7f0800d9;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/tensor/MyAdapter.java
+++ app/src/main/java/com/example/tensor/MyAdapter.java
@@ -1,12 +1,13 @@
 package com.example.tensor;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -5549,82 +5550,83 @@ public class MyAdapter extends BaseAdapter {
                 R.drawable.mob_icon5530,
                 R.drawable.mob_icon5531
     };
-    private Integer[] awaimage = {
-            0,
-            R.drawable.ka_skill_1,
-            R.drawable.ka_skill_2,
-            R.drawable.ka_skill_3,
-            R.drawable.ka_skill_4,
-            R.drawable.ka_skill_5,
-            R.drawable.ka_skill_6,
-            R.drawable.ka_skill_7,
-            R.drawable.ka_skill_8,
-            R.drawable.ka_skill_9,
-            R.drawable.ka_skill_10,
-            R.drawable.ka_skill_11,
-            R.drawable.ka_skill_12,
-            R.drawable.ka_skill_13,
-            R.drawable.ka_skill_14,
-            R.drawable.ka_skill_15,
-            R.drawable.ka_skill_16,
-            R.drawable.ka_skill_17,
-            R.drawable.ka_skill_18,
-            R.drawable.ka_skill_19,
-            R.drawable.ka_skill_20,
-            R.drawable.ka_skill_21,
-            R.drawable.ka_skill_22,
-            R.drawable.ka_skill_23,
-            R.drawable.ka_skill_24,
-            R.drawable.ka_skill_25,
-            R.drawable.ka_skill_26,
-            R.drawable.ka_skill_27,
-            R.drawable.ka_skill_28,
-            R.drawable.ka_skill_29,
-            R.drawable.ka_skill_30,
-            R.drawable.ka_skill_31,
-            R.drawable.ka_skill_32,
-            R.drawable.ka_skill_33,
-            R.drawable.ka_skill_34,
-            R.drawable.ka_skill_35,
-            R.drawable.ka_skill_36,
-            R.drawable.ka_skill_37,
-            R.drawable.ka_skill_38,
-            R.drawable.ka_skill_39,
-            R.drawable.ka_skill_40,
-            R.drawable.ka_skill_41,
-            R.drawable.ka_skill_42,
-            R.drawable.ka_skill_43,
-            R.drawable.ka_skill_44,
-            R.drawable.ka_skill_45,
-            R.drawable.ka_skill_46,
-            R.drawable.ka_skill_47,
-            R.drawable.ka_skill_48,
-            R.drawable.ka_skill_49,
-            R.drawable.ka_skill_50,
-            R.drawable.ka_skill_51,
-            R.drawable.ka_skill_52,
-            R.drawable.ka_skill_53,
-            R.drawable.ka_skill_54,
-            R.drawable.ka_skill_55,
-            R.drawable.ka_skill_56,
-            R.drawable.ka_skill_57,
-            R.drawable.ka_skill_58,
-            R.drawable.ka_skill_59,
-            R.drawable.ka_skill_60,
-            R.drawable.ka_skill_61,
-            R.drawable.ka_skill_62,
-            R.drawable.ka_skill_63,
-            R.drawable.ka_skill_64,
-            R.drawable.ka_skill_65,
-            R.drawable.ka_skill_66,
-            R.drawable.ka_skill_67,
-            R.drawable.ka_skill_68,
-            R.drawable.ka_skill_69,
-            R.drawable.ka_skill_70,
-            R.drawable.ka_skill_71,
-            R.drawable.ka_skill_72
+private Integer[] awaimage = {
+        0,
+        R.drawable.ka_skill_1,
+        R.drawable.ka_skill_2,
+        R.drawable.ka_skill_3,
+        R.drawable.ka_skill_4,
+        R.drawable.ka_skill_5,
+        R.drawable.ka_skill_6,
+        R.drawable.ka_skill_7,
+        R.drawable.ka_skill_8,
+        R.drawable.ka_skill_9,
+        R.drawable.ka_skill_10,
+        R.drawable.ka_skill_11,
+        R.drawable.ka_skill_12,
+        R.drawable.ka_skill_13,
+        R.drawable.ka_skill_14,
+        R.drawable.ka_skill_15,
+        R.drawable.ka_skill_16,
+        R.drawable.ka_skill_17,
+        R.drawable.ka_skill_18,
+        R.drawable.ka_skill_19,
+        R.drawable.ka_skill_20,
+        R.drawable.ka_skill_21,
+        R.drawable.ka_skill_22,
+        R.drawable.ka_skill_23,
+        R.drawable.ka_skill_24,
+        R.drawable.ka_skill_25,
+        R.drawable.ka_skill_26,
+        R.drawable.ka_skill_27,
+        R.drawable.ka_skill_28,
+        R.drawable.ka_skill_29,
+        R.drawable.ka_skill_30,
+        R.drawable.ka_skill_31,
+        R.drawable.ka_skill_32,
+        R.drawable.ka_skill_33,
+        R.drawable.ka_skill_34,
+        R.drawable.ka_skill_35,
+        R.drawable.ka_skill_36,
+        R.drawable.ka_skill_37,
+        R.drawable.ka_skill_38,
+        R.drawable.ka_skill_39,
+        R.drawable.ka_skill_40,
+        R.drawable.ka_skill_41,
+        R.drawable.ka_skill_42,
+        R.drawable.ka_skill_43,
+        R.drawable.ka_skill_44,
+        R.drawable.ka_skill_45,
+        R.drawable.ka_skill_46,
+        R.drawable.ka_skill_47,
+        R.drawable.ka_skill_48,
+        R.drawable.ka_skill_49,
+        R.drawable.ka_skill_50,
+        R.drawable.ka_skill_51,
+        R.drawable.ka_skill_52,
+        R.drawable.ka_skill_53,
+        R.drawable.ka_skill_54,
+        R.drawable.ka_skill_55,
+        R.drawable.ka_skill_56,
+        R.drawable.ka_skill_57,
+        R.drawable.ka_skill_58,
+        R.drawable.ka_skill_59,
+        R.drawable.ka_skill_60,
+        R.drawable.ka_skill_61,
+        R.drawable.ka_skill_62,
+        R.drawable.ka_skill_63,
+        R.drawable.ka_skill_64,
+        R.drawable.ka_skill_65,
+        R.drawable.ka_skill_66,
+        R.drawable.ka_skill_67,
+        R.drawable.ka_skill_68,
+        R.drawable.ka_skill_69,
+        R.drawable.ka_skill_70,
+        R.drawable.ka_skill_71,
+        R.drawable.ka_skill_72
 
-    };
+        };
+
 
     public MyAdapter(Context context){
         this.context=context;
@@ -5651,14 +5653,12 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        view = layoutInflater.inflate(R.layout.list_items,viewGroup,false);
+        view = layoutInflater.inflate(R.layout.mons_list_items,viewGroup,false);
         Mons_data monster=mons_data_list.get(i);
 
         ImageView monster_icon = (ImageView)view.findViewById(R.id.monster_icon);
         monster_icon.setImageResource(image[monster.getid()]);
 
-        ImageView awa1 = (ImageView)view.findViewById(R.id.awa1View);
-        awa1.setImageResource(awaimage[1]);
         ((TextView)view.findViewById(R.id.nameView)).setText(monster.getname());
         ((TextView)view.findViewById(R.id.statusView)).setText("HP "+String.valueOf(monster.gethp())+" 攻撃 "+String.valueOf(monster.getattack())+" 回復 "+String.valueOf(monster.getcure()));
         if(monster.gethp_110()!=0) {
@@ -5676,6 +5676,15 @@ public class MyAdapter extends BaseAdapter {
         ((TextView) view.findViewById(R.id.skill_name)).setText(monster.getskill_name());
         ((TextView) view.findViewById(R.id.skill_exp)).setText(monster.getskill_exp());
         ((TextView)view.findViewById(R.id.rareView)).setText("☆ "+String.valueOf(monster.getrare()));
+
+        LinearLayout awaView = view.findViewById(R.id.awaView);
+        for(String awa: monster.getawa()) {
+            ImageView awaImageView = new ImageView(context);
+            if(!awa.equals("no")) {
+                awaImageView.setImageResource(awaimage[Integer.parseInt(awa)]);
+                awaView.addView(awaImageView);
+            }
+        }
         return view;
     }
 
